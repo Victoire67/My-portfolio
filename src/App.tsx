22082ac4656f34024231b03f55323bbaa9b3c0b1
Victoire67 <ansimavicky@gmail.com>
2026-06-16 18:21:30 +0200

    component(landingtxt): designed landing texts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import type { ReactElement } from "react";
 import Header from "./components/Header";
+import LandingTxt from "./components/LandingTxt";
 function App(): ReactElement {
   return <div>
     <Header />
+    <LandingTxt />
   </div>
 }
 
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,7 @@
 import type { ReactElement } from "react";
 
 function Button({txt}:{txt:string}): ReactElement {
-    return <button className="bg-blue-600 px-4 text-lg font-semibold rounded-sm cursor-pointer">
+    return <button className="bg-blue-600 px-4 text-lg font-semibold rounded-sm cursor-pointer w-fit p-2">
         {txt}
     </button>
 }
