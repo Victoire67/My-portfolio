a0a293f2da9fd304bc163dc6229c4075c8792e36
Victoire67 <ansimavicky@gmail.com>
2026-06-16 18:42:03 +0200

    component(about): designed the section of the about section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import type { ReactElement } from "react";
 import Header from "./components/Header";
 import LandingTxt from "./components/LandingTxt";
+import About from "./components/About";
 function App(): ReactElement {
   return <div>
     <Header />
     <LandingTxt />
+    <About />
   </div>
 }
 
