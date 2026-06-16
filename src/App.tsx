4c98bdcd1791ae7c870a54f9eb209532105b61ea
Victoire67 <ansimavicky@gmail.com>
2026-06-16 22:37:00 +0200

    component(footer): designed the footer section

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,15 @@ import LandingTxt from "./components/LandingTxt";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contacts from "./components/Contacts";
+import Footer from "./components/Footer";
 function App(): ReactElement {
-  return <div className="grid gap-72 ">
+  return <div className="grid gap-84 ">
     <Header />
     <LandingTxt />
     <About />
     <Projects />
     <Contacts />
+    <Footer />
   </div>
 }
 
---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+function Footer(){
+    return <footer className="text-center">
+        Done in &copy; 2026 , by Victor with 💖
+    </footer>
+}
+
+export default Footer;
