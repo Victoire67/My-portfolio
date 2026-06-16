def3e7e4dd2938df6ae4d8448120d85d863f7ecf
Victoire67 <ansimavicky@gmail.com>
2026-06-16 18:22:39 +0200

    fix(imports): removed uneccessary import of the fragment react element

--- src/components/LandingTxt.tsx
+++ src/components/LandingTxt.tsx
@@ -1,5 +1,4 @@
 import Button from "./Button";
-import { Fragment } from "react/jsx-runtime";
 function LandingTxt() {
     return <div className="grid text-center items-center p-auto place-content-center mt-48">
         <h1 className="text-[64px]">Hello👋 ,  I am <span className="font-bold">Victoire Ansima <br /></span> I am a software engineer </h1>
