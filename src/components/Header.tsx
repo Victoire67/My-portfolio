6000ff0580bf06cb97be3eb18c35f58e0aaf8dd1
Victoire67 <ansimavicky@gmail.com>
2026-06-17 04:03:29 +0200

    fix(styling): fixed nav bar for small screens

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,12 @@
 import Button from "./Button";
 function Header() {
     return (<header className="bg-teal-900 flex items-center place-content-between bg-teal-00 w-full px-[20px] py-[10px] fixed">
-        <nav className="w-1/2">
-            <ul className="flex items-center place-content-left gap-16 text-2xl w-full">
+        <nav className="sm:w-1/2 w-full ">
+            <ul className="flex items-center sm:place-content-left sm:gap-16 sm:text-2xl gap-4 place-content-between w-full">
                 <li className="cursor-pointer hover:text-amber-200">Home</li>
                 <li className="cursor-pointer hover:text-amber-200">About</li>
                 <li className="cursor-pointer hover:text-amber-200">Projects</li>
-                <li className="cursor-pointer hover:text-amber-200">Contacts</li>
+                <li className="cursor-pointer hover:text-amber-200">Contacts</li>   
             </ul>
         </nav>
 
