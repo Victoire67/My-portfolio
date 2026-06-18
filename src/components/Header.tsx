575ea0e0ffea2571dd88f54b513af3b06af581f0
Victoire67 <ansimavicky@gmail.com>
2026-06-18 15:16:32 +0200

    fix(routing): fixed the routing with home scrolls

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ function Header() {
     return (<header className="bg-teal-900 flex items-center place-content-between bg-teal-00 w-full px-[20px] py-[10px] fixed">
         <nav className="sm:w-1/2 w-full ">
             <ul className="flex items-center sm:place-content-left sm:gap-8 sm:text-2xl gap-4 place-content-between w-full">
-                <a href="#Home">      <li className="cursor-pointer hover:text-amber-200">Home</li></a>
+                <a href="#home">      <li className="cursor-pointer hover:text-amber-200">Home</li></a>
                 <a href="#about">   <li className="cursor-pointer hover:text-amber-200">About</li></a>
                 <a href="#projects">                <li className="cursor-pointer hover:text-amber-200">Projects</li></a>
             </ul>
