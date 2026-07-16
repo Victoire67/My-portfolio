8a840641ba5d010851d3ea127b137e0f12562bc0
victoire67 <ansimavicky@gmail.com>
2026-07-16 22:15:54 +0200

    f

--- src/components/LandingTxt.tsx
+++ src/components/LandingTxt.tsx
@@ -5,7 +5,7 @@ function LandingTxt() {
         <h1 className="sm:text-[64px] text-[32px] z-50">Hello👋 ,  I am <span className="font-bold italic">Victoire Ansima <br className="hidden sm:block" /></span> I am a software engineer </h1>
 
         <div className="mx-auto">
-            <Button txt="View More" />
+            <Button txt="View More" href="4" />
         </div>
         <div className="h-100 w-100 bg-amber-200 absolute transform rotate-70 top-45 z-5 left-[80%] "></div>
     </div>
